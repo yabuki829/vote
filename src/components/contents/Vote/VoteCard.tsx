827c17768b70e2d543467f2c0621ccae87a2baac
yabuki829 <syodai829@icloud.com>
2022-10-03 14:30:48 +0900

    投票ui修正

--- src/components/contents/Vote/VoteCard.tsx
+++ src/components/contents/Vote/VoteCard.tsx
@@ -10,14 +10,18 @@ import { useCookies } from "react-cookie";
 
 
 const VoteCard:React.FC<Vote> = (props) => {
-  const { questionText,user,choices,id} = props
+  const { questionText,user,choices,id } = props
   const imageStyle = "w-10 h-10 border-2 rounded-full object-cover mr-4 shadow"
   const [cookies, setCookie, removeCookie] = useCookies()
   
   async function  handleVote(choiceID:string,text:string){
-    const token = cookies.token  
-    console.log("api通信を行います")
-    await putAPISelectChoice(choiceID,token,id)
+    if (!isVoted) {
+      const token = cookies.token  
+      console.log("api通信を行います")
+      await putAPISelectChoice(choiceID,token,id)
+      
+    }
+    
   }
   
   //投票していたら結果を表示する
@@ -52,17 +56,18 @@ const VoteCard:React.FC<Vote> = (props) => {
 
   // isVoted がtrue 投票済み
   // isChoice がtrue 投票した選択肢
-  const votedStyle = "absolute  whitespace-nowrap bg-blue-400 bg-opacity-40 text-left"
+  const votedStyle = "  whitespace-nowrap bg-blue-300  text-left"
   //            falseであれば投票してない選択肢
-  const votedStyle2 = "absolute  whitespace-nowrap bg-green-400 bg-opacity-40 text-left"
+  const votedStyle2 = "  whitespace-nowrap bg-gray-200 text-left"
 
   //         　falseで未投票
   const noVotedStyle =  ""
 
 
-const aa = "10"
+  const isVotedStyle = "flex relative justify-between cursor-default"
+  const isNotVotedStyle = "flex relative justify-between hover:bg-gray-200 "
 
-
+  const style = isVoted() ? isVotedStyle : isNotVotedStyle
   return (
     <div className="bg-white border shadow-lg rounded-lg mx-10 my-3 p-3 ">
       {/* 詳細画面に遷移する */}
@@ -84,16 +89,14 @@ const aa = "10"
           choices.map((choice)=>(
             //もし投稿していたらhover:色変わらない様にする
             <button onClick={(e) => handleVote(choice.id,choice.text)}  className='border border-gray-300 mb-2 w-full' key={choice.id}>
-              <div className='flex  justify-between bg-gray-100 hover:bg-gray-300'>
+              <div className={style}>         
                
+                <h1 className=' absolute text-left pl-2 bg-opacity-100'>{choice.text}</h1>
                 {
                   isVoted() && (
                     isChoice(choice) ?( <div className={votedStyle} style={ {width: "25%" ,color:"transparent"} }>こんな内側まで見ないでよ、えっち</div>) : ( <div className={votedStyle2} style={ {width: "25%" ,color:"transparent"} }>こんな内側まで見ないでよ、えっち</div>)
                   )  
                 }
-               
-                <h1 className='text-left pl-2 '>{choice.text}</h1>
-                
                 {
                   
                   //投票済みであればその選択肢が％を表示する
